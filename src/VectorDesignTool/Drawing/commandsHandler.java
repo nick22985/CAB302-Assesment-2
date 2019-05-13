c92a8a410430d7b5a7e6feb33de0e38c29fd48df
Nicholas Dempsey <n10224378@qut.edu.au>
2019-05-14 00:55:45 +1000

    Test Commit

--- src/VectorDesignTool/Drawing/commandsHandler.java
+++ src/VectorDesignTool/Drawing/commandsHandler.java
@@ -13,23 +13,24 @@ public class commandsHandler {
         for (int i = 0; i < command.length; i++) {
             String type = command[i][0].toString().replaceAll("\\[", "")
                     .replaceAll("\\]", "");
-            if (type.equals("LINE") || type.equals("RECTANGLE") || type.equals("PLOT")) {
+            if (type.equals("LINE") || type.equals("RECTANGLE") || type.equals("PLOT") || type.equals("ELLIPSE")) {
                 double x1 = Double.parseDouble(command[i][1].toString().replaceAll("\\[", "")
                         .replaceAll("\\]", ""));
                 double y1 = Double.parseDouble(command[i][2].toString().replaceAll("\\[", "")
                         .replaceAll("\\]", ""));
-
-                if (type.equals("LINE") || type.equals("RECTANGLE")) {
+                if (type.equals("LINE") || type.equals("RECTANGLE") || type.equals("ELLIPSE")) {
                     double x2 = Double.parseDouble(command[i][3].toString().replaceAll("\\[", "")
                             .replaceAll("\\]", ""));
                     double y2 = Double.parseDouble(command[i][4].toString().replaceAll("\\[", "")
                             .replaceAll("\\]", ""));
-
                     if (type.equals("LINE")) {
                         DrawLine(gc, x1, y1, x2, y2);
                     }
                     if (type.equals("RECTANGLE")) {
                         DrawRec(gc, x1, y1, x2, y2);
+                    }
+                    if (type.equals("ELLIPSE")) {
+                        DrawEllipse(gc, x1, y1, x2, y2);
                     }
                 }
                 if (type.equals("PLOT")) {
@@ -44,10 +45,8 @@ public class commandsHandler {
                 }
                 if (type.equals("FILL")) {
                     ChangeFillColour(Color.web(color));
-
                 }
             }
-
             else {
 
             }
@@ -120,6 +119,20 @@ public class commandsHandler {
      */
     public static void ChangePenColour(GraphicsContext gc, Color color) {
         gc.setStroke(color);
+    }
+
+    /**
+     *
+     * @param gc
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     */
+    public static void DrawEllipse(GraphicsContext gc, double x1, double y1, double x2, double y2) {
+        double x = gc.getCanvas().getHeight();
+        double y = gc.getCanvas().getWidth();
+        gc.strokeOval(x1 * x, y1 * y, x2 * x, y2 * y);
     }
 
     public static void ChangeFillColour(Color color) {
