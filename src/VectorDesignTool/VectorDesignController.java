7d49b008ab9f54685532080306d6c310cee0bfa1
Nicholas Dempsey <n10224378@qut.edu.au>
2019-05-14 00:43:36 +1000

    Test Commit

--- src/VectorDesignTool/VectorDesignController.java
+++ src/VectorDesignTool/VectorDesignController.java
@@ -7,9 +7,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
-import javafx.scene.shape.Line;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -20,14 +17,12 @@ public class VectorDesignController implements Initializable {
     @FXML
     private Canvas canvas;
     @FXML
-    GraphicsContext gc;
+    private GraphicsContext gc;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         gc = canvas.getGraphicsContext2D();
-
         ArrayList[][] command = vecLoad.LoadVecFile("Line.vec");
-
         commandsHandler.commandsHandler(gc, command);
     }
 
